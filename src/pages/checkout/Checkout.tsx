3e32ba78ce9956523a60c11ea604b216b959a320
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2024-02-16 16:07:39 +0100

    checkout page

--- src/pages/checkout/Checkout.tsx
+++ src/pages/checkout/Checkout.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../../context/shop-context';
+import { products } from '../../products';
+import { useNavigate } from 'react-router-dom';
 
-const Checkout = () => {
+const Checkout = () => {  
+  const {cartItem, getTotalAmount} = useContext(ShopContext);
+  const [deliverFee, setDeliveryFee] = useState<number | null>(null)
+  const [customFee, setCustomFee] = useState<number | null>(null)
+  const total = getTotalAmount()
+
+  const lengthItem = products.filter((product)=>{
+    if (cartItem[product.id] > 0){
+        return product
+    }
+})
+  function Delivery (): void{
+    if (lengthItem.length>= 5){
+      setDeliveryFee(Number(((.5/100)* total).toFixed(2)))
+    } else{
+      setDeliveryFee(0)
+    }
+  }
+
+  function Custom (): void{
+    if (total > 5000){
+      setCustomFee(Number(((.2/100)* total).toFixed(2)))
+    } else{
+      setCustomFee(Number(((5/100)* total).toFixed(2)))
+    }
+  }
+
+  useEffect(()=>{
+    Delivery()
+  },[])
+  useEffect(()=>{
+    Custom()
+  },[])
+
+
+  const navigate= useNavigate()
+
   return (
-    <div>
-        
+    <div className='checkout'>
+        <h2>Order Summary</h2>
+
+        <div className="order-total">
+          <div className="total-fees">
+            <p>items total ({lengthItem.length})</p>
+            <h5>${total}</h5>
+          </div>
+          <div className="total-fees">
+            <p>Delivery Fees</p>
+            <h5>${deliverFee}</h5>
+          </div>
+          <div className="total-fees">
+            <p>Custom Fees</p>
+            <h5>${customFee}</h5>
+          </div>
+          <div className="total-fees">
+            <p>Total</p>
+            <h5>${customFee && deliverFee ? total+customFee+deliverFee : total }</h5>
+          </div>
+        </div>
+
+        <button id='checkout'>Confirm Order</button>
     </div>
   )
 }
